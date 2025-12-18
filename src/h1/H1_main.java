fda298db78b5b648ebcdc469644fae1a81c05100
lennk <lennk@LennsWindows.fritz.box>
2025-12-18 12:17:48 +0100

    h1-finished

--- src/h1/H1_main.java
+++ src/h1/H1_main.java
@@ -2,6 +2,17 @@ package h1;
 
 public class H1_main {
     public static void main(String[] args) {
+		PrioListe liste = new PrioListe();
+		liste.addPatient(new Patient("A", 1));
+		liste.addPatient(new Patient("B", 10));
+		liste.addPatient(new Patient("C", 5));
+		liste.addPatient(new Patient("D", 3));
+		liste.addPatient(new Patient("E", 11));
+		liste.addPatient(new Patient("F", 7));
 		
+		liste.getNextPatient();
+		liste.getNextPatient();
+		
+		System.out.println("finished");
 	}
 }
--- src/h1/PrioListe.java
+++ src/h1/PrioListe.java
@@ -1,5 +1,36 @@
 package h1;
 
-public class PrioListe {
+import java.util.ArrayList;
 
+public class PrioListe {
+	public ArrayList<Patient> myList = new ArrayList<Patient>();
+
+	public void addPatient(Patient p) {
+
+		int stelle = 0;
+		for (Patient pt : myList) {
+			if (pt.prio > p.prio) {
+				stelle = myList.indexOf(pt);
+				break;
+			}
+			if(pt == myList.getLast() && pt.prio < p.prio) {
+				stelle = myList.indexOf(pt) + 1;
+			}
+		}
+		myList.add(stelle, p);
+
+	}
+	
+	public Patient getNextPatient() {
+		if (!myList.isEmpty()) {
+			Patient ergebnisPatient = myList.getFirst();
+			myList.remove(0);
+			return ergebnisPatient;
+		}
+		return null;
+	}
+	
+	public int getPosition(Patient p) {
+		return myList.indexOf(p);
+	}
 }
